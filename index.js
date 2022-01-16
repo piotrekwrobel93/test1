4a71c822471afeea6b1b9a2491df335602e6e30d
Peter <piotrwrobeldev@gmail.com>
2022-01-16 20:11:04 +0100

    added test

--- index.js
+++ index.js
@@ -7,15 +7,6 @@ app.get('/', (req, res) => {
     res.send({ message: 'Home page is here!' })
 })
 
-app.get('/api/v1/test', (req, res, next) => {
-    res.send({
-        data: {
-            type: 'text',
-            x: '10.33',
-            y: '26.72'
-        }
-    })
-})
 app.listen(5000, () => {
     console.log('Server is up and running...')
 })
