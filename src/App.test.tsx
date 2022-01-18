651dcc738bbeeb0305c31432c0e7cab1a500d5e9
Peter <piotrwrobeldev@gmail.com>
2022-01-18 22:57:45 +0100

    fix: test pass

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello world from react!/i);
+  const linkElement = screen.getByText(/Coding comes soon! Devops needs first :D/i);
   expect(linkElement).toBeInTheDocument();
 });
