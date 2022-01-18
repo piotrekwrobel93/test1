d7bd8ee6c73225c4204b953147b08cd8a552dc4c
Peter <piotrwrobeldev@gmail.com>
2022-01-18 22:54:31 +0100

    to test githb actions

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Coding comes soon! Devops needs first </h1>
+      <h1>Coding comes soon! Devops needs first :D</h1>
     </div>
   );
 }
