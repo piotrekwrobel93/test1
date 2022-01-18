0ad7162257ee3ea8ae098ffec257b58e48d1fba5
Peter <piotrwrobeldev@gmail.com>
2022-01-19 00:06:36 +0100

    changes to hello

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -12,7 +12,7 @@ export const Welcome :React.FC<IProps>= ({ title, name }: IProps) => {
     }, [])
     return(
         <div>
-            <p>Welcome :D
+            <p>Hello :D
             </p>
             { title && <p>{title}</p>}
             { name && <p>{name}</p>}
